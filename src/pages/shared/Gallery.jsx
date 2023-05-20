d00fafb50b4bda0e740912faf8d45720487bfad3
ShahSolaymanSinha <md.sinha.p@gmail.com>
2023-05-21 02:14:05 +0600

    gallery aos added

--- src/pages/shared/Gallery.jsx
+++ src/pages/shared/Gallery.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
 import GalleryCard from "./GalleryCard";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Gallery = ({ cars }) => {
+    AOS.init();
     return (
         <div className="mt-10">
             <div>
-                <h1 className="text-4xl font-bold mb-5 text-center">Our Best Selling Cars (Gallery)</h1>
+                <h1 data-aos="zoom-in" className="text-4xl font-bold mb-5 text-center">Our Best Selling Cars (Gallery)</h1>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-fr auto-cols-fr gap-10">
+            <div data-aos="zoom-out" className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-fr auto-cols-fr gap-10">
                 {cars.map((car) => (
                     <GalleryCard key={car._id} imageUrl={car?.picture}></GalleryCard>
                 ))}
