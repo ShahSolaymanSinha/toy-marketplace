ac157c57dba1d840a4e37b39d85313be4532f7e8
ShahSolaymanSinha <md.sinha.p@gmail.com>
2023-05-21 22:00:45 +0600

    some changes made

--- src/routes/PrivetRoute.jsx
+++ src/routes/PrivetRoute.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { AuthenticationContext } from "../providers/AuthenticationProvider";
+import Loader from "../pages/shared/Loader/Loader";
 
 const PrivateRoute = ({ children }) => {
     const { user, loading } = useContext(AuthenticationContext);
     const location = useLocation();
 
     if (loading) {
-        return "Loading";
+        return <Loader></Loader>;
     }
 
     if (user) {
